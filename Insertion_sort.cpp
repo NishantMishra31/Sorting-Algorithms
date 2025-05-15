e2b4199bdbe0b5849b3b2151c431185bd63cc720
Nishant Mishra <mnishant.work@gmail.com>
2025-05-16 00:57:52 +0530

    Update Insertion_sort.cpp

--- Insertion_sort.cpp
+++ Insertion_sort.cpp
@@ -1,6 +1,8 @@
 #include<bits/stdc++.h>
 using namespace std;
 
+// insertion sort has a time complexity of O(n²) in worst and average cases and O(n) in the best case
+
 void insertion_sort(int arr[], int n){
     for(int i = 0; i <= n-1; i++){
         int j = i;
