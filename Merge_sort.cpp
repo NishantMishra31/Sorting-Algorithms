6935fa1b5e7e976e749c64ab86e206aa3f3783c3
Nishant Mishra <mnishant.work@gmail.com>
2025-05-18 00:49:41 +0530

    Add files via upload

---
+++ Merge_sort.cpp
@@ -0,0 +1,52 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+void merge(vector <int> &arr, int low, int mid, int high){
+    vector <int> temp;
+    int left=low;
+    int right=mid+1;
+    while(left<=mid && right <= high){
+        if (arr[left]<=arr[right]){
+            temp.push_back(arr[left]);
+            left++;
+        }
+        else{
+            temp.push_back(arr[right]);
+            right++;
+        }
+    }
+    while(left<=mid){
+        temp.push_back(arr[left]);
+        left++;
+    }
+    while(right<=high){
+        temp.push_back(arr[right]);
+        right++;
+    }
+    for (int i = low; i <= high; i++){
+        arr[i] = temp[i-low];
+    }
+}
+void msort(vector <int> &arr, int low, int high){
+    if (low >= high) return;
+    int mid = (low+high)/2;
+    msort(arr, low, mid);
+    msort(arr, mid+1, high);
+    merge(arr, low, mid, high);
+}
+void merge_sort(vector <int> &arr, int n){
+    msort(arr, 0, n-1);
+}
+int main(){
+    int n;
+    cin >> n;
+    vector <int> arr(n);
+    for(int i = 0; i<n; i++){
+        cin >> arr[i];
+    }
+    merge_sort(arr,n);
+    for(int i = 0; i<n; i++){
+        cout << arr[i] << " ";
+    }
+    return 0;
+}
